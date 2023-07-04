96127c8b5721b78f46f579b212553a50e68e712b
mojsx <mojsx.dev@gmail.com>
2023-07-04 09:02:05 +0100

    chores: update POST request link.

--- src/pages/Cw.jsx
+++ src/pages/Cw.jsx
@@ -101,7 +101,7 @@ const Cw = () => {
     };
 
     axios
-      .post("http://localhost:5000/cw", data)
+      .post("https://vrpex-backend.onrender.com/cw", data)
       .then((res) => {
         setResult(res.data);
         setIsReady(true);
