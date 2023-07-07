e61da29ab6301cbbc260a9aa0201d3bfc8f3d9ae
mojsx <mojsx.dev@gmail.com>
2023-07-08 00:54:58 +0100

    chores: update loading text.

--- src/pages/Cw.jsx
+++ src/pages/Cw.jsx
@@ -140,7 +140,7 @@ const Cw = () => {
       <div className="bg-white grid grid-cols-3 h-[90vh] mt-5 place-content-center">
         <div className="col-start-2 col-end-3 mx-auto">
           <img src={Loading} alt="loading" />
-          <h1 className="text-5xl text-center mt-16">Loading...</h1>
+          <h1 className="text-5xl text-center mt-16">Calcul en cours...</h1>
         </div>
       </div>
     );
